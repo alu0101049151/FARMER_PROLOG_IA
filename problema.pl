5818eb710f40b1b6b37a573a8de32d89a8a18164
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2019-12-04 18:44:34 +0000

    Añadido Extraer

--- problema.pl
+++ problema.pl
@@ -9,19 +9,28 @@ orilla([oveja]).
 orilla([col]).
 % LA LISTA VACÍA??
 
+% Extrae un elmento de la cabeza de la lista.
+extraer(X,[X|T],T).
+extraer(X,[A|T],[A|R]):-extraer(X,T,R).
 
+% Calcula el número de elementos de la lista que le hemos pasado.
 acclen([],Acc,Length):-
-  Lenght = Acc.
+  Length = Acc.
 
-acclen([_|L], OldAcc, Length):
+acclen([_|L], OldAcc, Length):-
   NewAcc is OldAcc + 1 ,
-  acclen(L, NewAcc, Lenght).
+  acclen(L,NewAcc,Length).
 
-
-
+% Comprueba si la lista inicial tiene 4 elementos.
 comprobarInicio(ListaInicial):-
-  acclen(ListaInicial,0,Lenght), Lenght is 4.
+  acclen(ListaInicial,0,Length), Length is 4.
 
-/*solucion(ListaInicial, X) :-
+% Mueve un elemento de una lista a otra.
+mover(X,):-
+  extraer(X,)
+
+
+% Genera la solución del problema.
+solucion(ListaInicial, X) :-
   comprobarInicio(ListaInicial),  % Si se cumple que la ListaInicial contiene 4 elementos, continuamos.
-  mover(pastor,)*/
+%  mover(pastor,)
