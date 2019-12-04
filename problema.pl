bd640e36be4c695bb944cef19c30c497490798d1
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2019-12-04 18:21:51 +0000

    Comprueba que son 4 elementos en la lista

--- problema.pl
+++ problema.pl
@@ -1,4 +1,27 @@
-progenitor(clara, pedro).
-progenitor(pedro, jose).
-progenitor(ana, clara).
-progenitor(jose, clara).
+orilla([pastor, lobo, oveja, col]).
+orilla([lobo, col]).
+orilla([pastor, oveja]).
+orilla([pastor, lobo, col]).
+orilla([pastor, lobo, oveja]).
+orilla([pastor, oveja, col]).
+orilla([lobo]).
+orilla([oveja]).
+orilla([col]).
+% LA LISTA VACÍA??
+
+
+acclen([],Acc,Length):-
+  Lenght = Acc.
+
+acclen([_|L], OldAcc, Length):
+  NewAcc is OldAcc + 1 ,
+  acclen(L, NewAcc, Lenght).
+
+
+
+comprobarInicio(ListaInicial):-
+  acclen(ListaInicial,0,Lenght), Lenght is 4.
+
+/*solucion(ListaInicial, X) :-
+  comprobarInicio(ListaInicial),  % Si se cumple que la ListaInicial contiene 4 elementos, continuamos.
+  mover(pastor,)*/
