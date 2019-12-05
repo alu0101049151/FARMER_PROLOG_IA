8b0d48960d829648d76bbf8f0603889e0e03e80d
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2019-12-05 17:15:25 +0000

    Ya queda menos

--- problema.pl
+++ problema.pl
@@ -13,7 +13,7 @@ orilla([col]).
 extraer(X,[X|T],T).
 extraer(X,[A|T],[A|R]):-extraer(X,T,R).
 
-% Extrae varios elementos de la cabeza de la lista
+% Extrae 2 elementos de la cabeza de la lista
 extraer(X1,X2,[X1,X2|T],T).
 extraer(X1, X2, [A|T],[A|R]):-extraer(X1,X2,T,R).
 
@@ -36,36 +36,39 @@ comprobarInicio(ListaInicial):-
   acclen(ListaInicial,0,Length), Length is 4.
 
 % Etapa inicial
-configInicial([pastor,oveja,lobo,col],[]).
+configInicial(ListaInicial,ListaVacia):-
+  mover(ListaInicial,ListaVacia).
 
-% Mueve un elemento de una lista a otra. Extrae pastor y oveja
-mover([X1,X2, lobo, col], []):-
+% 1º Movimiento: Mueve un elemento de una lista a otra. Extrae pastor y oveja
+mover([X1,X2,lobo,col], []):-
   extraer(X1,X2,ListaInicial,T),  % [lobo,col]
-  insertar(X1,X2,ListaVacia,R).   % [pastor,oveja]
+  insertar(X1,X2,ListaVacia,R),   % [pastor,oveja]
+  mover(ListaInicial,ListaVacia).
 
-% Mueve el pastor que regresa solo
+% 2º Movimiento: Mueve el pastor que regresa solo
 mover([lobo,col], [X,oveja]):-
   extraer(X,ListaVacia,R),        % [oveja]
-  insertar(X,ListaInicial,T).     % [pastor,lobo,col]
+  insertar(X,ListaInicial,T),     % [pastor,lobo,col]
+  mover(ListaInicial,ListaVacia).
 
-% El pastor transporta al Lobo
-mover([X1,X2,col], [oveja]):-
+% 3º Movimiento: El pastor transporta al Lobo
+mover([X1,X2|col], [oveja]):-
   extraer(X1,X2,ListaInicial,T),  % [col]
   insertar(X1,X2,ListaVacia,R).   % [pastor,lobo,oveja]
 
-% El pastor regresa con la Oveja
+% 4º Movimiento: El pastor regresa con la Oveja
 mover([col], [X1,lobo,X2]):-
   extraer(X1,ListaVacia,R),       % [lobo,oveja]
   extraer(X2,ListaVacia,R),       % [lobo]
   insertar(X1,X2,ListaInicial,T). % [pastor,oveja,col]
 
-% El pastor transporta la col
+% 5º Movimiento: El pastor transporta la col
 mover([X1,oveja,X2],[lobo]):-
   extraer(X1,ListaInicial,T),     % [oveja,col]
   extraer(X2,ListaInicial,T),     % [oveja]
   insertar(X1,X2,ListaVacia,R).   % [pastor,col,lobo]
 
-% El pastor regresa solo
+% 6º Movimiento: El pastor regresa solo
 mover([oveja], [X,col,lobo]):-
   extraer(X,ListaVacia,R),        % [col,lobo]
   insertar(X,ListaInicial,T).     % [pastor,oveja]
